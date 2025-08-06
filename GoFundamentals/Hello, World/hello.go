25958b2317a37f11c4dc1295ab02a5e63fb7e11e
Beefy-py <hoftkenny@gmail.com>
2025-08-06 10:11:04 -0300

    feat: complete 'Hello, World'

--- GoFundamentals/Hello, World/hello.go
+++ GoFundamentals/Hello, World/hello.go
@@ -2,13 +2,31 @@ package main
 
 import "fmt"
 
+const SPANISH = "Spanish"
+const FRENCH = "French"
+
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
+const englishHelloPrefix = "Hello, "
 
 func HelloWorld() string {
-	return "Hello, World!";
+	return englishHelloPrefix + "World!"
 }
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, e%s!", name)
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	switch language {
+
+	case FRENCH:
+		return fmt.Sprintf("%s%s!", frenchHelloPrefix, name)
+	case SPANISH:
+		return fmt.Sprintf("%s%s", spanishHelloPrefix, name)
+	default:
+		return fmt.Sprintf("%s%s!", englishHelloPrefix, name)
+	}
 }
 
 func main() {
--- GoFundamentals/Hello, World/hello_test.go
+++ GoFundamentals/Hello, World/hello_test.go
@@ -11,11 +11,38 @@ func TestHelloWorld(t *testing.T) {
 	}
 }
 
-func TestHello(t *testing.T){
-	expected := "Hello, Kenny!";
-	result := Hello("Kenny");
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		result := Hello("Kenny", "")
+		expected := "Hello, Kenny!"
 
+		assertCorrectMessage(t, result, expected)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		result := Hello("", "")
+		expected := "Hello, World!"
+
+		assertCorrectMessage(t, result, expected)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		result := Hello("Elodie", "Spanish")
+		expected := "Hola, Elodie"
+		assertCorrectMessage(t, result, expected)
+
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		result := Hello("François", "French")
+		expected := "Bonjour, François!"
+		assertCorrectMessage(t, result, expected)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, result, expected string) {
+	t.Helper()
 	if result != expected {
-		t.Errorf("Expected %q, but got %q", expected, result)	;
+		t.Errorf("Expected %q, but got %q", result, expected)
 	}
 }
